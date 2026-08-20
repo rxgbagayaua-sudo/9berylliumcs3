d43af77775025c01dff2d7501830d71619c7222e
rxgbagayaua-sudo <rxgbagayaua@brc.pshs.edu.ph>
2026-08-21 02:54:06 +0800

    Commit Code of zodiacBerylliumBagayaua

--- q1/zodiacBerylliumBagayaua.py
+++ q1/zodiacBerylliumBagayaua.py
@@ -1,31 +1,6 @@
 birthyear = int(input("Enter your birth year: "))
-
+zodiacsign = ["Rat (鼠 / Shǔ)","Ox (牛 / Niú)","Tiger (虎 / Hǔ)","Rabbit (兔 / Tù)","Dragon (龙 / Lóng)","Snake (蛇 / Shé)","Horse (马 / Mǎ)","Goat (羊 / Yáng)","Monkey (猴 / Hóu)","Rooster (鸡 / Jī)","Dog (狗 / Gǒu)","Pig (猪 / Zhū)"]
 if birthyear < 1900:
     print("Invalid year, it should not be earlier than 1900")
 else:
-    zodiacsign = (birthyear - 1900) % 12
-
-    if zodiacsign == 0:
-        print("Your Chinese Zodiac Sign is: Rat (鼠 / Shǔ)")
-    elif zodiacsign == 1:
-        print("Your Chinese Zodiac Sign is: Ox (牛 / Niú)")
-    elif zodiacsign == 2:
-        print("Your Chinese Zodiac Sign is: Tiger (虎 / Hǔ)")
-    elif zodiacsign == 3:
-        print("Your Chinese Zodiac Sign is: Rabbit (兔 / Tù)")
-    elif zodiacsign == 4:
-        print("Your Chinese Zodiac Sign is: Dragon (龙 / Lóng)")
-    elif zodiacsign == 5:
-        print("Your Chinese Zodiac Sign is: Snake (蛇 / Shé)")
-    elif zodiacsign == 6:
-        print("Your Chinese Zodiac Sign is: Horse (马 / Mǎ)")
-    elif zodiacsign == 7:
-        print("Your Chinese Zodiac Sign is: Goat (羊 / Yáng)")
-    elif zodiacsign == 8:
-        print("Your Chinese Zodiac Sign is: Monkey (猴 / Hóu)")
-    elif zodiacsign == 9:
-        print("Your Chinese Zodiac Sign is: Rooster (鸡 / Jī)")
-    elif zodiacsign == 10:
-        print("Your Chinese Zodiac Sign is: Dog (狗 / Gǒu)")
-    elif zodiacsign == 11:
-        print("Your Chinese Zodiac Sign is: Pig (猪 / Zhū)")
+    print(f"Your Chinese Zodiac Sign is: {zodiacsign[(birthyear-1900)%12]}")
