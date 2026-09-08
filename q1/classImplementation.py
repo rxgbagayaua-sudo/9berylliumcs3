fd646724c16e19b242547f7d94e3f7b8f865000c
rxgbagayaua-sudo <rxgbagayaua@brc.pshs.edu.ph>
2026-09-08 22:24:14 +0800

    Initialize the attributes

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -1,1 +1,7 @@
+class clubs:
+    def __init__(self, name, adviser, noOfMembers, activeStatus):
+        self.name = name
+        self.adviser = adviser
+        self.__noOfMembers = noOfMembers
+        self.activeStatus = activeStatus
 
