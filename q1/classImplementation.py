492f85f454675f7fd6e67a06a20dc4e4a86d906f
rxgbagayaua-sudo <rxgbagayaua@brc.pshs.edu.ph>
2026-09-16 20:20:24 +0800

    Add the rest of the Code

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -15,6 +15,21 @@ class clubs:
      print(Activity, "has been scheduled for", self.name)
 
     def updateMembers(self, amount):
-        if self.__noOFMembers 
+        self.__noOfMembers += amount
 
     def getNoOfMembers(self):
+       return self.__noOfMembers
+
+club1 = clubs("Polaris", "Gerald Paz", 40, True)
+club2 = clubs("Agham Radio Club", "JC Bartolae", 20, True)
+
+print("---BEFORE---")
+print("Object 1: ", club1.name,"-",club1.adviser, "-", club1.getNoOfMembers(), "members")
+print("Object 2: ", club2.name,"-",club2.adviser, "-", club2.getNoOfMembers(), "members")
+
+print("\nRemoving 3 members from", club1.name)
+club1.updateMembers(-3)
+
+print("\n---AFTER---")
+print("Object 1: ", club1.name,"-",club1.adviser, "-", club1.getNoOfMembers(), "members")
+print("Object 2: ", club2.name,"-",club2.adviser, "-", club2.getNoOfMembers(), "members")
